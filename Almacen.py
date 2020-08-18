fdd8f1236ce1cee78b0b43f3eb071f8fe3b48d03
Toresp <mikillterminator@gmail.com>
2020-08-18 14:24:12 +0200

    Cambios menores, establecidas las bases en cuanto al testeo de musica random

--- Almacen.py
+++ Almacen.py
@@ -1,6 +1,13 @@
 import random
-with open("Canciones.txt","r") as file:
 
-generos = {'chill' : chill, 'rock' :rock,'dubsteb' : dubsteb}
-txt='dubsteb'
-print (generos[txt][random.randint(0,4)])
+
+def style(msg):
+    msg.lower()
+    music_type = {'chill':'Canciones.csv','dubsteb':'CancionesD.cvs','rock':'CancionesR.csv'}
+    value = music_type[msg]
+    with open(value,'r') as link:
+        links = link.read().split(';')
+        print(links[random.randint(0, len(links))-1])
+    return 0
+
+
